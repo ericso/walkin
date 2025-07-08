163485a3cfc06aa0850a026120ecbca128d409d1
Eric So <me@eric.so>
2025-07-08 16:57:58 -0700

    remove commented out code

--- character/character.gd
+++ character/character.gd
@@ -5,23 +5,6 @@ extends Node2D
 
 var is_stepping := false
 
-#func step_forward():
-	#if is_stepping:
-		#return
-	#
-	#$AnimatedSprite2D.play("walk")
-	#
-	#is_stepping = true
-	#var target_pos = position + Vector2(step_distance, 0)
-	#
-	#var tween = create_tween()
-	#tween.tween_property(self, "position", target_pos, step_duration).set_trans(Tween.TRANS_SINE).set_ease(Tween.EASE_OUT)
-	#tween.finished.connect(_on_step_finished)
-#
-#func _on_step_finished():
-	#is_stepping = false
-	#$AnimatedSprite2D.stop()
-
 func step_forward():
 	$AnimatedSprite2D.play("walk")
 
