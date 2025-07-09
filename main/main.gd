362b10605c281934f2ecb45b7b671c7bcb7eb893
Eric So <me@eric.so>
2025-07-08 17:06:25 -0700

    Revert "add a step conter that counts on clicks (#2)"
    This reverts commit 6ce4b1ac78a049202c9109e156e58e723d83af7e.

--- main/main.gd
+++ main/main.gd
@@ -2,15 +2,10 @@ extends Node2D
 
 @onready var character = $Character
 @onready var camera = $Camera
-@onready var step_label = $UI/StepLabel
-
-var step_count := 0
 
 func _unhandled_input(event):
 	if event is InputEventMouseButton and event.pressed:
 		character.step_forward()
-		step_count += 1
-		step_label.text = "Steps: %d" % step_count
 
 func _process(_delta):
 	camera.position.x = character.position.x + 200  # keep camera ahead of player
