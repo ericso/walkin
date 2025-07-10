dbc8376e32b04f6980972d13b332d77e08c9f7d8
Eric So <me@eric.so>
2025-07-09 22:05:32 -0700

    create singletons for tracking steps and managing save state

--- scenes/main/main.gd
+++ scenes/main/main.gd
@@ -6,11 +6,21 @@ extends Node2D
 
 var step_count := 0
 
+func _ready():
+	StepTracker.steps_updated.connect(_on_steps_updated)
+	SaveManager.load()
+	
 func _unhandled_input(event):
 	if event is InputEventMouseButton and event.pressed and event.button_index == MOUSE_BUTTON_LEFT:
 		character.step_forward()
-		step_count += 1
-		step_label.text = "Steps: %d" % step_count
+		StepTracker.add_steps(1)
 
 func _process(_delta):
 	camera.position.x = character.position.x + 200  # keep camera ahead of player
+
+func _on_steps_updated(_new_total: int):
+	step_label.text = StepTracker.get_steps_label()
+
+func _notification(what: int) -> void:
+	if what == NOTIFICATION_WM_CLOSE_REQUEST:
+		SaveManager.save()
